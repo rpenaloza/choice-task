100b56c5ba5976690aef1efccb9eb231c46fabe0
Rodrigo Peñaloza <rod.penaloza@gmail.com>
2023-11-14 09:45:26 -0600

    Initial commit rest project

---
+++ rest/src/main/java/com/encora/choice/App.java
@@ -0,0 +1,13 @@
+package com.encora.choice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
